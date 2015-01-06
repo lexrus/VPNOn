b9c0d34afb8090ff6c6457a3a59f63dfcf7d8451
Lex Tang <lexrus@gmail.com>
2015-01-06 17:43:23 +0800

    Add about view controller.

---
+++ VPNOn/LTVPNAboutViewController.swift
@@ -0,0 +1,22 @@
+//
+//  LTVPNAboutViewController.swift
+//  VPNOn
+//
+//  Created by Lex Tang on 1/6/15.
+//  Copyright (c) 2015 LexTang.com. All rights reserved.
+//
+
+import UIKit
+
+class LTVPNAboutViewController : UIViewController {
+    
+    @IBOutlet weak var versionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let version = NSBundle.mainBundle().objectForInfoDictionaryKey("CFBundleShortVersionString") as String? {
+            versionLabel.text = version
+        }
+    }
+}
