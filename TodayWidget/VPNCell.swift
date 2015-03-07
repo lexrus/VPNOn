4dcc844c69f0f3fc0d6c73b305f6dc7e4ce3ce48
lexrus <lexrus@gmail.com>
2015-03-07 23:40:34 +0800

    Adjust colors of latency.

--- TodayWidget/VPNCell.swift
+++ TodayWidget/VPNCell.swift
@@ -35,14 +35,14 @@ class VPNCell: UICollectionViewCell {
     
     var colorOfLatency: UIColor {
         get {
-            var latencyColor = UIColor(red:0.39, green:0.68, blue:0.19, alpha:1)
+            var latencyColor = UIColor(red: 0.5, green: 0.8, blue: 0.19, alpha: 1)
             
             if latency > 200 {
-                latencyColor = UIColor(red:0.8, green:0.54, blue:0.21, alpha:1)
+                latencyColor = UIColor(red: 0.7, green: 0.5, blue: 0.0, alpha: 1)
             } else if latency > 500 {
-                latencyColor = UIColor(red:1 , green:0.11, blue:0.34, alpha:1)
+                latencyColor = UIColor(red: 1, green: 0.11, blue: 0.34, alpha: 1)
             } else if latency == -1 {
-                latencyColor = UIColor(white: 0.5, alpha: 1)
+                latencyColor = UIColor(white: 0.4, alpha: 1)
             }
             
             return latencyColor
@@ -64,6 +64,18 @@ class VPNCell: UICollectionViewCell {
         if latency == -1 {
             return
         }
+        
+        var lineRect = flagImageView.frame
+        lineRect.origin.y = lineRect.origin.y - 6
+        lineRect.size.height = 2
+        let dotSpacing: CGFloat = 3
+        lineRect.origin.x += dotSpacing
+        lineRect.size.width -= dotSpacing * 2
+        
+        var rectanglePath = UIBezierPath(roundedRect: lineRect, cornerRadius: 2)
+        
+        colorOfLatency.setFill()
+        rectanglePath.fill()
     }
     
     func configureWithVPN(vpn: VPN, selected: Bool = false) {
@@ -88,11 +100,11 @@ class VPNCell: UICollectionViewCell {
     }
     
     func updateTitleColor() {
-        var statusColor = UIColor(white: 0.5, alpha: 1)
+        var statusColor = UIColor(white: 1, alpha: 0.5)
         
         switch status {
         case .Connected:
-            statusColor = UIColor(red:0, green:0.75, blue:1, alpha:1)
+            statusColor = UIColor(red: 0, green: 0.75, blue: 1, alpha: 1)
             break
         case .Connecting:
             statusColor = UIColor.yellowColor()
