44e1ff8657aa0e3574b2e7c013f4df077c5ae0bb
lexrus <lexrus@gmail.com>
2015-03-30 23:46:18 +0800

    Complete Glance interface.

--- VPN On WatchKit Extension/InterfaceController.swift
+++ VPN On WatchKit Extension/InterfaceController.swift
@@ -9,72 +9,14 @@
 import WatchKit
 import Foundation
 import VPNOnKit
-import CoreData
-import NetworkExtension
 
 let kVPNOnSelectedIDInToday = "kVPNOnSelectedIDInToday"
 
-class InterfaceController: WKInterfaceController {
-    var vpns: [VPN] {
-        get {
-            return VPNDataManager.sharedManager.allVPN()
-        }
-    }
+class InterfaceController: VPNInterfaceController {
     
     @IBOutlet weak var tableView: WKInterfaceTable!
-    
-    var selectedID: String? {
-        get {
-            return NSUserDefaults.standardUserDefaults().objectForKey(kVPNOnSelectedIDInToday) as String?
-        }
-        set {
-            if let newID = newValue {
-                NSUserDefaults.standardUserDefaults().setObject(newID, forKey: kVPNOnSelectedIDInToday)
-            } else {
-                NSUserDefaults.standardUserDefaults().removeObjectForKey(kVPNOnSelectedIDInToday)
-            }
-            
-        }
-    }
-
-    override func awakeWithContext(context: AnyObject?) {
-        super.awakeWithContext(context)
-        
-        NSNotificationCenter.defaultCenter().addObserver(
-            self,
-            selector: Selector("coreDataDidSave:"),
-            name: NSManagedObjectContextDidSaveNotification,
-            object: nil)
-        
-        NSNotificationCenter.defaultCenter().addObserver(
-            self,
-            selector: Selector("VPNStatusDidChange:"),
-            name: NEVPNStatusDidChangeNotification,
-            object: nil)
-        
-        NSNotificationCenter.defaultCenter().addObserver(
-            self,
-            selector: Selector("pingDidUpdate:"),
-            name: "kLTPingDidUpdate",
-            object: nil)
-        
-        NSNotificationCenter.defaultCenter().addObserver(self,
-            selector: Selector("didTurnOnVPN:"),
-            name: kDidTurnOnVPN,
-            object: nil)
-        
-        NSNotificationCenter.defaultCenter().addObserver(self,
-            selector: Selector("didTurnOffVPN:"),
-            name: kDidTurnOffVPN,
-            object: nil)
-    }
-    
-    deinit {
-        NSNotificationCenter.defaultCenter().removeObserver(self)
-    }
 
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
         super.willActivate()
         
         loadVPNs()
@@ -116,38 +58,6 @@ class InterfaceController: WKInterfaceController {
     func didTurnOffVPN(notification: NSNotification) {
         VPNManager.sharedManager.disconnect()
         loadVPNs()
-    }
-    
-    func connectVPN(vpn: VPN) {
-        let passwordRef = VPNKeychainWrapper.passwordForVPNID(vpn.ID)
-        let secretRef = VPNKeychainWrapper.secretForVPNID(vpn.ID)
-        let certificate = VPNKeychainWrapper.certificateForVPNID(vpn.ID)
-        let titleWithSubfix = "Widget - \(vpn.title)"
-        
-        if vpn.ikev2 {
-            VPNManager.sharedManager.connectIKEv2(titleWithSubfix,
-                server: vpn.server,
-                account: vpn.account,
-                group: vpn.group,
-                alwaysOn: vpn.alwaysOn,
-                passwordRef: passwordRef,
-                secretRef: secretRef,
-                certificate: certificate)
-        } else {
-            VPNManager.sharedManager.connectIPSec(titleWithSubfix,
-                server: vpn.server,
-                account: vpn.account,
-                group: vpn.group,
-                alwaysOn: vpn.alwaysOn,
-                passwordRef: passwordRef,
-                secretRef: secretRef,
-                certificate: certificate)
-        }
-    }
-
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
     }
     
     // MARK: - Notification
